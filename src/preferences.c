60097caaca901160f6ae0881c16381336e26ad98
brenon.alexis@gmail.com <brenon.alexis@gmail.com@a214742a-d7f9-290b-63af-bf51a10d8b95>
2011-04-06 09:58:42 +0000

    Documentation remaniée. Module preferences commencé.

---
+++ src/preferences.c
@@ -0,0 +1,10 @@
+/**
+  * @file preferences.c
+  * @brief Fichier d'implémentation du module \em preferences.
+  *
+  * Module de manipulation des préférences de l'application.
+  * @author Alexis BRENON in STELLODY TEAM
+  */
+
+#include "preferences.h"
+
