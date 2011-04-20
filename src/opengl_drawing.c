aea4de48a2e4963c30e44bd3f56261a7d4b5b668
matthieu.vidal.69@gmail.com <matthieu.vidal.69@gmail.com@a214742a-d7f9-290b-63af-bf51a10d8b95>
2011-04-20 09:08:51 +0000

    Mise à jour : OpenGLDrawing

--- src/opengl_drawing.c
+++ src/opengl_drawing.c
@@ -359,8 +359,8 @@ static void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) ;
 
-	drawSphere() ;
-	/* drawTetraedre() ; */
+	/* drawSphere() ; */
+	drawTetraedre() ;
 	/* drawCube() ; */
 
 	glFlush() ;
