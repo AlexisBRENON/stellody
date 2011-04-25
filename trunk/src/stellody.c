5458db6e01fef9d7eb5f8dfa6edb512683600e8b
brenon.alexis@gmail.com <brenon.alexis@gmail.com@a214742a-d7f9-290b-63af-bf51a10d8b95>
2011-04-25 19:07:05 +0000

    BUG du module preferences

--- trunk/src/stellody.c
+++ trunk/src/stellody.c
@@ -6,6 +6,7 @@
   */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #if defined(__linux)
 #include <fmodex/fmod.h>
@@ -35,27 +36,66 @@ int main (int argc, char* argv[])
     GtkBuilder* builder = NULL;
     GtkWidget* win = NULL;
 
-    if (argc > 1 && strcmp(argv[1], "-x") == 0)
+    int iAnswer = -1;
+
+	printf ("Que voulez-vous faire ?\n\n");
+	printf ("\t1 : preferencesRegressionTest\n");
+	printf ("\t2 : analyzedTracksRegressionTest\n");
+	printf ("\t3 : analyzedTrackRegressionTest\n");
+	printf ("\t4 : filesRegressionTest\n");
+	printf ("\t5 : openglRegressionTest\n");
+	printf ("\t9 : Test interface\n");
+	printf ("\n\t0 : Quitter\n");
+
+    while (iAnswer < 0 || iAnswer > 9)
     {
-        gtk_init(&argc, &argv);
-
-        builder = gtk_builder_new();
-        gtk_builder_add_from_file(builder, "data/windows/Window.glade",
-                                  NULL);
-        gtk_builder_connect_signals(builder, NULL);
-
-        win = GTK_WIDGET(gtk_builder_get_object(builder,
-                                                "Stellody_Window"));
-        gtk_widget_show_all(win);
-        gtk_main();
+    	printf("\n Choix : ");
+    	scanf("%d", &iAnswer);
     }
-    else
+
+    switch (iAnswer)
     {
-        printf("Returned value : %d\n", filesRegressionTest());
-        printf("Returned value : %d\n",
+    	case 0:
+			break;
+		case 1:
+			printf ("\nReturned value : %d\n",
+					preferencesRegressionTest());
+			break;
+		case 2:
+			printf ("\nReturned value : %d\n",
+					analyzedTracksRegressionTest());
+			break;
+		case 3:
+			printf ("\nReturned value : %d\n",
+					analyzedTrackRegressionTest());
+			break;
+		case 4:
+			printf ("\nReturned value : %d\n",
+					filesRegressionTest());
+			break;
+		case 5:
+			printf("Returned value : %d\n",
                OpenGLDrawingRegressionTest(&argc, argv));
+			break;
+		case 9:
+			gtk_init(&argc, &argv);
+
+			builder = gtk_builder_new();
+			gtk_builder_add_from_file(builder, "data/windows/Window.glade",
+									NULL);
+			gtk_builder_connect_signals(builder, NULL);
+
+			win = GTK_WIDGET(gtk_builder_get_object(builder,
+                                                "Stellody_Window"));
+			gtk_widget_show_all(win);
+			gtk_main();
+			break;
+		default:
+			printf ("Choix incorrect...");
+			break;
     }
 
 
     return 0;
 }
+
