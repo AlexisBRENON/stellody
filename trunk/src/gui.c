828684bd7a2de3559b044fcadb0ff6dc8cab5317
brenon.alexis@gmail.com <brenon.alexis@gmail.com@a214742a-d7f9-290b-63af-bf51a10d8b95>
2011-04-30 08:26:07 +0000

    Trop déchire, presque une vraie application musicale. Je suis fan ! Y'a plus qu'a rajouter le Stellarium et le module d'analyse. Je pense qu'on est large. J'ai abandonné les tests valgrind lorsque GTK m'en a sorties 3700+.

--- trunk/src/gui.c
+++ trunk/src/gui.c
@@ -7,7 +7,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <assert.h>
+#include <ctype.h>
 #include <gtk/gtk.h>
 
 #if defined(__linux)
@@ -37,6 +39,8 @@ GUI contenant la base pour le Stellarium. */
 fichier GUI contenant la gestion des préférences. */
 #define GUI_ABOUT "data/windows/About.glade" /**< Nom du fichier GUI
 contenant le A Propos. */
+#define GUI_ADDTRACK "data/windows/AddTrack.glade" /**< Interface de
+chargement d'un fichier. */
 
 #define GUI_MAIN_WIN "Stellody_Window" /**< Nom du widget de niveau
 supérieur. */
@@ -47,6 +51,8 @@ stellarium. */
 #define GUI_PREFERENCES_WIN "Preferences_VBox" /**< Nom du conteneur des
 preferences. */
 #define GUI_ABOUT_WIN "About_VBox" /**< Nom du conteneur de A Propos; */
+#define GUI_ADDTRACK_WIN "AddTrack_VBox" /**< Conteneneur d'ajout de
+fichier. */
 
 
 /* ********************************************************************* */
@@ -115,6 +121,47 @@ static int guiUnparent (gpointer* pData)
 	return EXIT_SUCCESS;
 }
 
+/**
+  * @fn static gboolean guiTrackScaleIncrement (gpointer* pData)
+  * @brief Déplace le curseur durant la lecture d'un morceau.
+  *
+  * @param pData Le tableau de données.
+  * @return TRUE si le morceau est encore en lecture, FALSE sinon
+  */
+static gboolean guiTrackScaleIncrement (gpointer* pData)
+{
+	GtkWidget* pScale = NULL;
+	GtkObject* pAdjustment = NULL;
+	double dTrackPosition = 0;
+	double dLength = 0;
+	gboolean bIsPlaying = FALSE;
+	gboolean bIsPaused = FALSE;
+
+	FMOD_Channel_IsPlaying((FMOD_CHANNEL*)pData[PLAYING_CHANNEL],
+								&bIsPlaying);
+	FMOD_Channel_GetPaused((FMOD_CHANNEL*)pData[PLAYING_CHANNEL],
+								&bIsPaused);
+
+	if (bIsPlaying == TRUE && bIsPaused == FALSE)
+	{
+		pScale = GTK_WIDGET(gtk_builder_get_object(pData[MAIN_BUILDER],
+														"Track_Scale"));
+		dTrackPosition = gtk_range_get_value(GTK_RANGE(pScale));
+		gtk_range_set_value(GTK_RANGE(pScale), dTrackPosition+1);
+	}
+
+
+	pAdjustment = GTK_OBJECT(gtk_builder_get_object(pData[MAIN_BUILDER],
+												"Track_Adjustment"));
+	dLength = gtk_adjustment_get_upper(GTK_ADJUSTMENT(pAdjustment));
+
+	if (dTrackPosition+1 >= dLength || bIsPlaying == FALSE)
+	{
+		on_Stop_Action_activate(NULL, pData);
+	}
+
+	return bIsPlaying;
+}
 /* ********************************************************************* */
 /*                                                                       */
 /*                   Fonctions relatives à la fenêtre                    */
@@ -179,22 +226,38 @@ int on_Play_Action_activate (GtkWidget* psWidget, gpointer* pData)
 	FMOD_BOOL bIsPlaying = FALSE;
 	const char* strPath;
 	AnalyzedTrack* psTrack = NULL;
+	GtkWidget* pScale = NULL;
+	GtkObject* pAdjustment = NULL;
 	int i = 1;
+	unsigned int uiTrackPosition = 0;
+	unsigned int uiTrackLength = 0;
 
-	if (pData[FMOD_MY_CHANNEL] != NULL)
+	if (pData[PLAYING_CHANNEL] != NULL)
 	{
-		FMOD_Channel_IsPlaying((FMOD_CHANNEL*)pData[FMOD_MY_CHANNEL],
+		FMOD_Channel_IsPlaying((FMOD_CHANNEL*)pData[PLAYING_CHANNEL],
 								&bIsPlaying);
 	}
 	if (bIsPlaying == FALSE)
 	{
+		/* On récupère à partir de quelle partie commencer le morceau */
+		pScale = GTK_WIDGET(gtk_builder_get_object(pData[MAIN_BUILDER],
+													"Track_Scale"));
+		uiTrackPosition = (unsigned int)
+						gtk_range_get_value(GTK_RANGE(pScale));
+
+		pAdjustment = GTK_OBJECT(gtk_builder_get_object(
+												pData[MAIN_BUILDER],
+												"Track_Adjustment"));
+
+		/* Si aucun morceau n'est dans la playlist... */
 		if (pData[PLAYLIST] == NULL)
 		{
+			/* On charge la premier morceau qu'on trouve */
 			do
 			{
 				psTrack = analyzedTracksGetTrack(pData[ANALYZED_TRACKS], i);
 				i++;
-			} while (psTrack == NULL);
+			} while (psTrack == NULL && i < 100);
 
 			strPath = analyzedTrackGetPath(psTrack);
 			FMOD_System_CreateSound((FMOD_SYSTEM*) pData[FMOD_CONTEXT],
@@ -202,11 +265,32 @@ int on_Play_Action_activate (GtkWidget* psWidget, gpointer* pData)
 									FMOD_CREATESTREAM | FMOD_SOFTWARE,
 									NULL,
 									&pSound);
+			/* On le fait commencer au moment voulu */
+			FMOD_Sound_GetLength(pSound, &uiTrackLength, FMOD_TIMEUNIT_MS);
+			uiTrackPosition = (uiTrackPosition*uiTrackLength)/100;
+
+			gtk_adjustment_set_upper(GTK_ADJUSTMENT(pAdjustment),
+									(double) uiTrackLength/1000);
+			gtk_range_set_value(GTK_RANGE(pScale),
+								(double) uiTrackPosition/1000);
+		}
+		else
+		{
+			/** @todo Si la playlist n'est pas vide. */
 		}
 
+		/* On joue le morceau. */
+		g_timeout_add_seconds(1,
+							(GSourceFunc) guiTrackScaleIncrement,
+							pData);
+
 		FMOD_System_PlaySound((FMOD_SYSTEM*) pData[FMOD_CONTEXT],
-							FMOD_CHANNEL_FREE, pSound, FALSE,
-							(FMOD_CHANNEL**)&(pData[FMOD_MY_CHANNEL]));
+							FMOD_CHANNEL_FREE, pSound, TRUE,
+							(FMOD_CHANNEL**)&(pData[PLAYING_CHANNEL]));
+		FMOD_Channel_SetPosition((FMOD_CHANNEL*)pData[PLAYING_CHANNEL],
+						uiTrackPosition, FMOD_TIMEUNIT_MS);
+		FMOD_Channel_SetPaused((FMOD_CHANNEL*)pData[PLAYING_CHANNEL],
+								FALSE);
 	}
 
 	return EXIT_SUCCESS;
@@ -214,7 +298,20 @@ int on_Play_Action_activate (GtkWidget* psWidget, gpointer* pData)
 
 int on_Stop_Action_activate (GtkWidget* psWidget, gpointer* pData)
 {
-	FMOD_Channel_Stop((FMOD_CHANNEL*) pData[FMOD_MY_CHANNEL]);
+	GtkWidget* pRange = NULL;
+	GtkObject* pAdjustment = NULL;
+
+	FMOD_Channel_Stop((FMOD_CHANNEL*) pData[PLAYING_CHANNEL]);
+	pData[PLAYING_CHANNEL] = NULL;
+
+	pAdjustment = GTK_OBJECT(gtk_builder_get_object(
+											pData[MAIN_BUILDER],
+											"Track_Adjustment"));
+	pRange = GTK_WIDGET(gtk_builder_get_object(pData[MAIN_BUILDER],
+												"Track_Scale"));
+
+	gtk_adjustment_set_upper(GTK_ADJUSTMENT(pAdjustment), 0);
+	gtk_range_set_value(GTK_RANGE(pRange), 0);
 
 	return EXIT_SUCCESS;
 }
@@ -223,9 +320,9 @@ int on_Pause_Action_activate (GtkWidget* psWidget, gpointer* pData)
 {
 	FMOD_BOOL bIsPaused;
 
-	FMOD_Channel_GetPaused((FMOD_CHANNEL*) pData[FMOD_MY_CHANNEL],
+	FMOD_Channel_GetPaused((FMOD_CHANNEL*) pData[PLAYING_CHANNEL],
 							&bIsPaused);
-	FMOD_Channel_SetPaused(pData[FMOD_MY_CHANNEL],
+	FMOD_Channel_SetPaused(pData[PLAYING_CHANNEL],
 							!bIsPaused);
 
 	return EXIT_SUCCESS;
@@ -233,14 +330,114 @@ int on_Pause_Action_activate (GtkWidget* psWidget, gpointer* pData)
 
 int on_AddTrack_Action_activate (GtkWidget* psWidget, gpointer* pData)
 {
-	printf ("Ajout morceau\n");
+	GtkWidget* pDialog;
+	GtkWidget* pParent;
+	int iDialogAnswer;
+
+
+	/* Affichage de la fenêtre de sélection de fichier. */
+	pParent = GTK_WIDGET(gtk_builder_get_object(pData[MAIN_BUILDER],
+										GUI_MAIN_WIN));
+	pDialog = gtk_file_chooser_dialog_new ("Ajouter un morceau...",
+				      GTK_WINDOW(pParent),
+				      GTK_FILE_CHOOSER_ACTION_OPEN,
+				      GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+				      GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT,
+				      NULL);
+	iDialogAnswer = gtk_dialog_run (GTK_DIALOG (pDialog));
+	gtk_widget_hide_all (pDialog);
+
+
+	/* Si l'utilisateur a ouvert un fichier */
+	if (iDialogAnswer == GTK_RESPONSE_ACCEPT)
+	{
+		char* strFilename;
+		char* strExtension;
+		int iLength;
+		int i;
+
+		strFilename = gtk_file_chooser_get_filename(
+											GTK_FILE_CHOOSER (pDialog));
+		strExtension = strrchr(strFilename, '.');
+		iLength = strlen(strExtension);
+
+		for (i = 0; i<iLength; i++)
+		{
+			strExtension[i] = tolower(strExtension[i]);
+		}
+
+		/* On vérifie que ce soit un fichier pris en charge */
+		if (strcmp(strExtension, ".mp3") == 0 ||
+			strcmp(strExtension, ".wma") == 0 ||
+			strcmp(strExtension, ".mid") == 0 ||
+			strcmp(strExtension, ".m3u") == 0 ||
+			strcmp(strExtension, ".mp2") == 0 ||
+			strcmp(strExtension, ".ogg") == 0 ||
+			strcmp(strExtension, ".raw") == 0 ||
+			strcmp(strExtension, ".wav") == 0)
+		{
+			printf ("Nouveau morceau ajouté :\n");
+			printf ("\t%s\n", strFilename);
+		}
+		else
+		{
+			GtkWidget* pErrorDialog;
+
+			pErrorDialog = gtk_message_dialog_new_with_markup(
+										GTK_WINDOW(pParent),
+										GTK_DIALOG_MODAL,
+										GTK_MESSAGE_WARNING,
+										GTK_BUTTONS_CLOSE,
+		"<b>Erreur :</b>\n\tLe fichier %s n'est pas reconnu.",
+										strFilename);
+			gtk_dialog_run(GTK_DIALOG(pErrorDialog));
+
+			gtk_widget_destroy(pErrorDialog);
+		}
+
+		g_free (strFilename);
+	}
+
+	gtk_widget_destroy (pDialog);
 
 	return EXIT_SUCCESS;
 }
 
 int on_AddDir_Action_activate (GtkWidget* psWidget, gpointer* pData)
 {
-	printf ("Ajout répertoire\n");
+	GtkWidget* pDialog;
+	GtkWidget* pParent;
+	int iDialogAnswer;
+
+
+	/* Affichage de la fenêtre de sélection de fichier. */
+	pParent = GTK_WIDGET(gtk_builder_get_object(pData[MAIN_BUILDER],
+										GUI_MAIN_WIN));
+	pDialog = gtk_file_chooser_dialog_new ("Ajouter un dossier...",
+				      GTK_WINDOW(pParent),
+				      GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER,
+				      GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+				      GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT,
+				      NULL);
+	iDialogAnswer = gtk_dialog_run (GTK_DIALOG (pDialog));
+	gtk_widget_hide_all (pDialog);
+
+
+	/* Si l'utilisateur a ouvert un fichier */
+	if (iDialogAnswer == GTK_RESPONSE_ACCEPT)
+	{
+		char* strFoldername;
+
+		strFoldername = gtk_file_chooser_get_filename(
+											GTK_FILE_CHOOSER (pDialog));
+
+		preferencesAddFilesPath((Preferences*) pData[PREFERENCES],
+								strFoldername);
+
+		g_free (strFoldername);
+	}
+
+	gtk_widget_destroy (pDialog);
 
 	return EXIT_SUCCESS;
 }
@@ -346,7 +543,22 @@ int on_About_Action_activate (GtkWidget* psWidget, gpointer* pData)
 	return EXIT_SUCCESS;
 }
 
+int on_Track_Scale_value_changed (GtkWidget* psWidget,
+								GdkEventButton* event,
+								 gpointer* pData)
+{
+	gdouble dNewTrackPosition;
 
+	if (pData[PLAYING_CHANNEL] != NULL)
+	{
+		dNewTrackPosition = gtk_range_get_value(GTK_RANGE(psWidget));
+		FMOD_Channel_SetPosition((FMOD_CHANNEL*) pData[PLAYING_CHANNEL],
+									dNewTrackPosition*1000,
+									FMOD_TIMEUNIT_MS);
+	}
+
+	return EXIT_SUCCESS;
+}
 /* ********************************************************************* */
 /*                           FENÊTRE PREFERENCES                         */
 /* ********************************************************************* */
