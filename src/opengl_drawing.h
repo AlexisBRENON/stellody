cf473d25f3cd5d8d966f0117c1d346cd2d09c299
brenon.alexis@gmail.com <brenon.alexis@gmail.com@a214742a-d7f9-290b-63af-bf51a10d8b95>
2011-05-17 08:36:45 +0000

    Gestion click, scroll, clavier

--- src/opengl_drawing.h
+++ src/opengl_drawing.h
@@ -66,7 +66,7 @@ typedef struct sOpenGLData OpenGLData;
 /* ********************************************************************* */
 
 gboolean drawingButtonMouse (GtkWidget * psWidget,
-							 GdkEventButton * psEvent,
+							 GdkEventAny * psEvent,
 							 gpointer * pData) ;
 
 gboolean drawingMotionMouse (GtkWidget * psWidget,
