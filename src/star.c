0ff397cbbec6986a87e03ce8e5b012428a1af6c0
matthieu.vidal.69@gmail.com <matthieu.vidal.69@gmail.com@a214742a-d7f9-290b-63af-bf51a10d8b95>
2011-05-19 14:11:26 +0000

    OpenGL buggé

--- src/star.c
+++ src/star.c
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <gtk/gtk.h>
 #include "star.h"
 #include "analyzed_track.h"
 
@@ -24,22 +25,89 @@
 /*                                                                       */
 /* ********************************************************************* */
 
-int starCreate(Star * pStar, const AnalyzedTrack * pTrack)
+int starCreate(Star * pStar, const AnalyzedTrack * pTrack, GPtrArray * psExistingStars)
 {
+	int i = 0 ;
+	int iTest = 0 ;
+	int iCounter = 0 ;
+	int iArraySize = 0 ;
 	float fAverage = 0 ;
 	float fMedian = 0 ;
+	float fValue1 = 0 ;
+	float fValue2 = 0 ;
+	float * fTemp ;
+
+	iArraySize = psExistingStars->len ;
 
 	fAverage = analyzedTrackGetFrequenciesAverage(pTrack) ;
 	fMedian = analyzedTrackGetFrequenciesMedian(pTrack) ;
-
-	pStar->fSize = 0.5 ;
-	pStar->fColourR = 1 ;
-	pStar->fColourG = 1 ;
-	pStar->fColourB = 1 ;
-	pStar->fPositionX = 0 ;
-	pStar->fPositionY = 0 ;
+	
+	pStar->fPositionX = (int) fAverage ;
+	pStar->fPositionY = (int) fMedian ;	
 	pStar->fPositionZ = 0 ;
+	
+	/* Vérifie que les coordonnées ne sont pas encore prises. */
+	while (iTest == 0)
+	{
+		iTest = 1 ;
+		for (i = 0 ; i < iArraySize && iTest == 1 ; i ++)
+		{
+			fTemp = g_ptr_array_index(psExistingStars, i) ;
+		
+			if (pStar->fPositionX == fTemp[0] && pStar->fPositionY == fTemp[1] && pStar->fPositionZ == fTemp[2])
+			{
+				iTest = 0 ;
+				if (iCounter%3 == 0)
+				{
+					pStar->fPositionX = pStar->fPositionX + 1 ;
 
+				}
+				else if (iCounter%3 == 1)
+				{
+					pStar->fPositionY = pStar->fPositionY + 1 ;
+
+				}
+				else
+				{
+					/* Incrément du z, quand il sera mobile. */
+				}
+
+				iCounter = iCounter + 1 ;
+			}
+		}
+	}
+	
+	/* Ajoute les nouvelles coordonnées aux données existantes. */
+	
+	fTemp = (float *) malloc(3*sizeof(float)) ;
+	fTemp[0] = pStar->fPositionX ;
+	fTemp[1] = pStar->fPositionY ;
+	fTemp[2] = pStar->fPositionZ ;
+	
+	g_ptr_array_add(psExistingStars, fTemp) ;
+	
+
+	/* Définition de la taille. */
+	
+	fValue1 = analyzedTrackGetIemeFrequenciesValues(pTrack, 0) ;
+	fValue2 = analyzedTrackGetIemeFrequenciesValues(pTrack, 7) ;
+	
+	pStar->fSize = 0.5 * (1 - fValue1)*(1 - fValue1)*(1 - fValue2)*(1 - fValue2) ;
+	
+	/* Définition des couleurs. */
+	
+	fValue1 = analyzedTrackGetIemeFrequenciesValues(pTrack, 1) ;
+	fValue2 = analyzedTrackGetIemeFrequenciesValues(pTrack, 6) ;
+	pStar->fColourR = (1 - fValue1)*(1 - fValue1)*(1 - fValue2)*(1 - fValue2) ;
+	
+	fValue1 = analyzedTrackGetIemeFrequenciesValues(pTrack, 2) ;
+	fValue2 = analyzedTrackGetIemeFrequenciesValues(pTrack, 5) ;
+	pStar->fColourG = (1 - fValue1)*(1 - fValue1)*(1 - fValue2)*(1 - fValue2) ;
+	
+	fValue1 = analyzedTrackGetIemeFrequenciesValues(pTrack, 3) ;
+	fValue2 = analyzedTrackGetIemeFrequenciesValues(pTrack, 4) ;
+	pStar->fColourB = (1 - fValue1)*(1 - fValue1)*(1 - fValue2)*(1 - fValue2) ;
+	
 	return (0) ;
 }
 
